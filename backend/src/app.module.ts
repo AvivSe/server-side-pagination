817c501e5bf6a05c03e43f730376d18eaa8af952
avivsegal <avivsegal@gmail.com>
2019-12-28 08:49:39 +0200

    moving directories

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,16 @@
-import { Module } from '@nestjs/common';
-import { MongooseModule } from '@nestjs/mongoose';
-import { SongsModule } from './songs.module';
+import {Module} from '@nestjs/common';
+import {MongooseModule} from '@nestjs/mongoose';
+import {SongsModule} from './songs.module';
+import {join} from 'path';
+import {ServeStaticModule} from '@nestjs/serve-static';
 
 @Module({
-  imports: [MongooseModule.forRoot('mongodb://localhost/nest'), SongsModule],
-  controllers: [],
-  providers: [],
+    imports: [MongooseModule.forRoot(process.env.MONGO_CONNECTION_STRING), SongsModule, ServeStaticModule.forRoot({
+        rootPath: join(__dirname, 'frontend'),
+        renderPath: '*',
+    })],
+    controllers: [],
+    providers: [],
 })
-export class AppModule {}
+export class AppModule {
+}
